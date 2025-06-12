f8ef46ba3a512c2c290a53276c1fb294c858d85a
Ja-sonYun <killa30867@gmail.com>
2025-06-12 20:48:17 +0900

    Add project base

---
+++ README.md
@@ -0,0 +1,2 @@
+# Security Checker
+
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              uv
+              rye
+            ];
+          };
+          shellHook = ''
+            source .venv/bin/activate
+          '';
+        }
+      );
+    };
+}
